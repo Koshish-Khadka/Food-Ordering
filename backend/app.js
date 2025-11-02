6c01d5446a98ad0fcc21692bed8f0ac0078e046d
koshish khadka <koshishkhadka364@gmail.com>
2025-11-02 13:07:04 +0545

    setup backend and frontend

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./utils/Database.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+dotenv.config({ quiet: true });
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// api create
+
+// api.use("/api/users", userRoutes);
+
+
+app.get("/", (req, res) => {
+  res.send("Hello Koshish don !");
+});
+
+// Server listening
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running at port http://localhost:${PORT}`);
+});
