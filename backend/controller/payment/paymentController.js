8393d23c625d27cfdacfdd766b54bcdaabbeb116
koshish khadka <koshishkhadka364@gmail.com>
2025-12-16 10:50:59 +0545

    khalti payment completed

--- backend/controller/payment/paymentController.js
+++ backend/controller/payment/paymentController.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import Order from "../../models/orderModel";
+import Order from "../../models/orderModel.js";
 
 // this function initiate khalti payment process
 export const initiateKhaltiPayment = async (req, res) => {
@@ -10,7 +10,7 @@ export const initiateKhaltiPayment = async (req, res) => {
       .json({ message: "Order ID and amount are required." });
   }
   const data = {
-    return_url: "http://localhost:3000/api/payment/success ",
+    return_url: "http://localhost:3000/api/payment/success",
     purchase_order_id: orderId,
     amount: amount,
     website_url: "http://localhost:3000",
@@ -27,11 +27,18 @@ export const initiateKhaltiPayment = async (req, res) => {
   );
 
   //
-  console.log(response);
+  let order = await Order.findById(orderId);
+  order.paymentDetails.pidx = response.data.pidx;
+  await order.save();
+
+  //   add the pidx to the order database
+
+  console.log(response.data);
 };
 
 // This function verify khalti payment
 export const verifyKhaltiPayment = async (req, res) => {
+  console.log("Verify khalti payment is running");
   // query params bata lenu paryo like from:
   //http://localhost:3000/?pidx=tNwtLJebHHXnnMNAPLfKoV&transaction_id=bjXoJ2jNZNQy8BFYzSmQgZ&tidx=bjXoJ2jNZNQy8BFYzSmQgZ&txnId=bjXoJ2jNZNQy8BFYzSmQgZ&amount=1000&total_amount=1000&mobile=98XXXXX002&status=Completed&purchase_order_id=123&purchase_order_name=OrderName123
   // from this we need pidx so,
@@ -48,19 +55,11 @@ export const verifyKhaltiPayment = async (req, res) => {
   );
   if (response.data.status === "Completed") {
     // make changes to the database
-    const order = await Order.findById(orderId);
-    if (!order) {
-      res.status(404).json({ message: "Order not found" });
-    }
-    const updateOrder = await Order.findByIdAndUpdate(
-      orderId,
-      {
-       paymentDetails.method="Khalti",
-       paymentDetails.status="paid",
-       orderStatus="ontheway",
-      },
-      { new: true }
-    );
+    const order = await Order.find({ "paymentDetails.pidx": pidx });
+    // update the order
+    order[0].paymentDetails.method = "Khalti";
+    order[0].paymentDetails.status = "paid";
+    await order[0].save();
   } else {
     res.redirect("http://localhost:3000/payment/failure");
   }
--- backend/routes/user/paymentRoute.js
+++ backend/routes/user/paymentRoute.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { initiateKhaltiPayment } from "../../controller/payment/paymentController.js";
+import {
+  initiateKhaltiPayment,
+  verifyKhaltiPayment,
+} from "../../controller/payment/paymentController.js";
 import { isAuthenticated } from "../../middleware/isAuthenticated.js";
 import { catchAsync } from "../../services/catchAsync.js";
 const router = express.Router();
 
 router.route("/").post(isAuthenticated, catchAsync(initiateKhaltiPayment));
+router.route("/success").get(catchAsync(verifyKhaltiPayment));
 
 export default router;
